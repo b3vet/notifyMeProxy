0908234eca32ee223cc09a3e3e4b6a60c713bf27
Berke Üçvet <berke@24saatteis.com>
2022-02-23 15:55:08 +0300

    fixes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require('express'),
     bodyParser = require('body-parser'),
     axios = require('axios'),
@@ -6,28 +7,24 @@ var express = require('express'),
 app.use(bodyParser.json());
 
 app.all('*', function (req, res, next) {
-
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "https://b3vet.github.io");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
     if (req.method === 'OPTIONS') {
-        // CORS Preflight
         res.send();
     } else {
-        var targetURL = req.header('Target-URL');
+        let targetURL = req.header('Target-URL');
         if (!targetURL) {
             res.status(500).send({ error: 'There is no Target-URL header in the request' });
             return;
         }
-        axios.get(targetURL, {
-            body: req.body,
-            headers: req.header('Authorization') ? {'Authorization': req.header('Authorization')} : {}
-        }).then((response) => {
-            res.send(response)
+        if (!targetURL.includes("http")) {
+            targetURL = `https://${targetURL}`;
+        }
+        axios.get(targetURL).then((response) => {
+            res.status(response.status).send(response.data);
         }).catch((error) => {
-            console.error(`error: ${error}`)
             res.status(400).send({error})
         })
     }
